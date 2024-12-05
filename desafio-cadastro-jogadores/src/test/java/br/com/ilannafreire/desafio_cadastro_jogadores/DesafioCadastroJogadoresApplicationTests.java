5a9e35d00dd86bde0d62dc6cfcfbfda24efba96b
Ilanna Freire <84036601+ilannafreire@users.noreply.github.com>
2024-12-04 21:53:16 -0300

    config

---
+++ desafio-cadastro-jogadores/src/test/java/br/com/ilannafreire/desafio_cadastro_jogadores/DesafioCadastroJogadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ilannafreire.desafio_cadastro_jogadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCadastroJogadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
